ec3fe1de541ca444ab21e9eb98365f76c4a65c00
Mostafa <anonymostafa@protonmail.ch>
2021-11-06 23:36:01 +0330

    Nodejs website is wonderful!

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const http = require('http');
+    const express = require('express')
+    const index = express()
+    const port = 3000
+
+    index.get('/', (req, res) => {
+      res.send('Welcome to Anna-Emma Metaverse shop!')
+    })
+
+    index.get('/about', (req, res) => {
+      res.send('About')
+    })
+
+    index.get('/home', (req, res) => {
+      res.send('Home')
+    })
+
+    index.get('/contact-us', (req, res) => {
+      res.send('Contact')
+    })
 
 
-http.createServer(function name(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-  }).listen(8080); 
+    index.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+    })
